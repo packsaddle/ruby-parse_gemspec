bb4a134ece8aded29280aa9c49abee50dd5fb415
sanemat <o.gata.ken@gmail.com>
2015-09-23 00:45:54 +0900

    chore(parser): use Tempfile.open instead of Tempfile.create
    Tempfile.create is Ruby 2.1.0's feature

--- lib/parse_gemspec/parser.rb
+++ lib/parse_gemspec/parser.rb
@@ -2,10 +2,16 @@ module ParseGemspec
   class Parser
     def self.parse(contents)
       spec = nil
-      Tempfile.create('parse_gemspec') do |f|
-        f.write contents
-        f.rewind
-        spec = Gem::Specification.load(f.path)
+      begin
+        temp_file = Tempfile.open('parse_gemspec') do |f|
+          f.write contents
+          f.flush
+          f.rewind
+          spec = Gem::Specification.load(f.path)
+          f
+        end
+      ensure
+        temp_file.close!
       end
       {
         name: spec.name,
