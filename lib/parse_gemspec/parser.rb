643e6d3b7da270088b80d1cb79bbb81dc769403a
sanemat <o.gata.ken@gmail.com>
2015-09-23 00:59:27 +0900

    style(parser): fix for rubocop

--- lib/parse_gemspec/parser.rb
+++ lib/parse_gemspec/parser.rb
@@ -1,22 +1,23 @@
 module ParseGemspec
   class Parser
     def self.parse(contents)
-      spec = nil
-      begin
-        temp_file = Tempfile.open('parse_gemspec') do |f|
-          f.write contents
-          f.flush
-          f.rewind
-          spec = Gem::Specification.load(f.path)
-          f
-        end
-      ensure
-        temp_file.close!
-      end
+      spec = build_spec(contents)
       {
         name: spec.name,
         version: spec.version.version
       }
+    end
+
+    def self.build_spec(gemspec)
+      temp_file, spec = Tempfile.open('parse_gemspec') do |f|
+        f.write gemspec
+        f.flush
+        f.rewind
+        [f, Gem::Specification.load(f.path)]
+      end
+      spec
+    ensure
+      temp_file.close!
     end
   end
 end
