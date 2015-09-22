85fa3db1218432d33425536f9b9b145e65edbd29
sanemat <o.gata.ken@gmail.com>
2015-09-23 02:44:24 +0900

    chore(test): test git based fixture

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -14,5 +14,18 @@ module ParseGemspec
         end
       end
     end
+
+    sub_test_case 'rubocop-select' do
+      gemspec = File.read('./test/fixtures/rubocop-select/rubocop-select.gemspec')
+      expected = {
+        name: 'rubocop-select',
+        version: '2.2'
+      }
+      test '.parse' do
+        assert do
+          Parser.parse(gemspec) == expected
+        end
+      end
+    end
   end
 end
