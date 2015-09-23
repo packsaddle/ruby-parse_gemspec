c15902dc92ed7aab3bead6fdc84403f4581468ac
sanemat <o.gata.ken@gmail.com>
2015-09-23 16:40:02 +0900

    style(format): fix for rubocop

--- test/test_specification.rb
+++ test/test_specification.rb
@@ -2,8 +2,13 @@ require_relative 'helper'
 
 module ParseGemspec
   class TestSpecification < Test::Unit::TestCase
+    fixture_path = File.join('.', 'test', 'fixtures')
     sub_test_case 'gem as is (bigdecimal)' do
-      gemspec_path = './test/fixtures/bigdecimal-1.2.7/bigdecimal.gemspec'
+      gemspec_file = File.join(
+        'bigdecimal-1.2.7',
+        'bigdecimal.gemspec'
+      )
+      gemspec_path = File.join(fixture_path, gemspec_file)
       expected = {
         name: 'bigdecimal',
         version: '1.2.7'
@@ -16,7 +21,11 @@ module ParseGemspec
     end
 
     sub_test_case 'git repository (rubocop-select)' do
-      gemspec_path = './test/fixtures/rubocop-select/rubocop-select.gemspec'
+      gemspec_file = File.join(
+        'rubocop-select',
+        'rubocop-select.gemspec'
+      )
+      gemspec_path = File.join(fixture_path, gemspec_file)
       expected = {
         name: 'rubocop-select',
         version: '0.1.2.pre.beta'
@@ -29,7 +38,11 @@ module ParseGemspec
     end
 
     sub_test_case 'gem git command included (checkstyle_filter-git)' do
-      gemspec_path = './test/fixtures/checkstyle_filter-git-1.0.2/checkstyle_filter-git.gemspec'
+      gemspec_file = File.join(
+        'checkstyle_filter-git-1.0.2',
+        'checkstyle_filter-git.gemspec'
+      )
+      gemspec_path = File.join(fixture_path, gemspec_file)
       expected = {
         name: 'checkstyle_filter-git',
         version: '1.0.2'
